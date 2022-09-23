60d3bc311cf3a844905268d897d0ba50339300d0
Ashley <ashuhleyt@gmail.com>
2022-09-23 13:31:46 -0700

    finalized setup

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,7 @@
-file = File.read(ARGV[0], 'r')
-count = file.length
+file = File.open(ARGV[0], 'r') #permission to read
+# require 'pry'; binding.pry#open said file and read it. 
+count = file.read #actually reading it. 
 #manipulations 
-
-puts "Created '#{ARGV[1]}' containing #{count} characters"
+new_file = File.open(ARGV[1], 'w') #take info that program does(converting) and will allow me to write to the file. 
+new_file.write(count)
+puts "Created '#{ARGV[1]}' containing #{count} characters."
