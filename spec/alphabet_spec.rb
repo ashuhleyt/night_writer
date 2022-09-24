8bef8f8167b3411cd4d6221edb8b03d6987305cc
Ashley <ashuhleyt@gmail.com>
2022-09-23 17:11:38 -0700

    feat: alphabet to braille

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Alphabet do
   it 'exists' do 
     expect(@alphabet).to be_instance_of(Alphabet)
   end
+
+  it 'contains letters of the alphabet' do 
+    expect(@alphabet.to_braille("a")).to eq(["0.", "..", ".."])
+    expect(@alphabet.to_braille("o")).to eq(["0.", ".0", "0."])
+    expect(@alphabet.to_braille("i")).to eq([".0", "0.", ".."])
+    expect(@alphabet.to_braille("w")).to eq([".0", "00", ".0"])
+    expect(@alphabet.to_braille(" ")).to eq(["..", "..", ".."])
+  end
 end
---
+++ spec/english_braille_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require './lib/english_braille'
+require './lib/alphabet'
+
+RSpec.describe EnglishBraille do 
+  before(:each) do 
+    @EnglishBraille 
+  end
+end
