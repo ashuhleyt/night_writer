a771d952f9bf68736c8e2a71c3468ad4b1e017d0
Ashley <ashuhleyt@gmail.com>
2022-09-27 15:06:23 -0700

    test

--- spec/english_braille_spec.rb
+++ spec/english_braille_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe EnglishBraille do
 
   it 'converts to individual braille characters' do 
     expect(@EnglishBraille.convert_to_braille("a", [["0.", "..", ".."]])).to eq(["0.", "..", ".."])
+    expect(@EnglishBraille.convert_to_braille("s", [[".0", "0.", "0."]])).to eq([".0", "0.", "0."])
     expect(@EnglishBraille.convert_to_braille("sleep", [[".0", "0.", "0.", "0.", "0.", "0.", "0.", ".0", "..", "0.", ".0", "..", "00", "0.", "0."]])).to eq([".0", "0.", "0.", "0.", "0.", "0.", "0.", ".0", "..", "0.", ".0", "..", "00", "0.", "0."])
   end
 
