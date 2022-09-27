88ec928b5eda9b9cdf1d8f2570d5f941abf8f9d4
Ashley <ashuhleyt@gmail.com>
2022-09-26 19:22:29 -0700

    :feat: linebreak

--- lib/english_braille.rb
+++ lib/english_braille.rb
@@ -1,53 +1,32 @@
 class EnglishBraille
   include Library 
 
-  # def initialize
-
-  # end
-
-  def to_braille(string)
-    letters = string.chars
-    changed_letters = []
-    letters.each do |letter| 
-      changed_letters << @braille[letter]
-    end
-    changed_letters.flatten 
-  
-    row1 = []
-    row2 = []
-    row3 = []
-
-    changed_letters.each do |letter|
-      row1 << changed_letters[0][0]
-      row2 << changed_letters[0][1]
-      row3 << changed_letters[0][2]
-    end
-     [row1.join, row2.join, row3.join]
-  end
-
   def convert_string_to_braille(message)
+    collector = {}
     final_message = ""
     braille_letters = []
+    counter = 0
+    new_counter = -1
     message_chars = message.downcase.chars
     message_chars.each do |char|
-      braille_letters << self.to_braille(char)
+      braille_letters << @braille[char]
     end
-    for i in 0..2 do 
-      braille_letters.each do |letter|
-        final_message.concat(letter[i])
+    loop do 
+      collector[counter] = braille_letters.shift(40)
+      break if braille_letters.count == 0
+      counter += 1
+    end 
+    until new_counter == counter
+      new_counter += 1
+      for i in 0..2 do 
+        collector[new_counter].each do |letter|
+          final_message.concat(letter[i])
+        end
+        final_message.concat("\n")
       end
-      final_message.concat("\n") 
     end
-    final_message
+      final_message
   end
-
-  def line_break(converted_message)
-    File.open 
-    braille_message = []
-
-    braille_message << converted_message
-    if braille_message.delete("\n").length >=30
-      # line break 
-    end
-  end 
 end
+
+
--- spec/english_braille_spec.rb
+++ spec/english_braille_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
-require './lib/english_braille'
-require './lib/alphabet'
+
 
 RSpec.describe EnglishBraille do 
   before(:each) do 
@@ -11,24 +10,13 @@ RSpec.describe EnglishBraille do
     expect(@EnglishBraille).to be_instance_of(EnglishBraille)
   end
 
-  it 'populates braille equivalent' do
-    expect(@library.to_braille("a")).to eq(["0.", "..", ".."])
-    expect(@library.to_braille("o")).to eq(["0.", ".0", "0."])
-    expect(@library.to_braille("i")).to eq([".0", "0.", ".."])
-    expect(@library.to_braille("w")).to eq([".0", "00", ".0"])
-    expect(@library.to_braille(" ")).to eq(["..", "..", ".."])
-  end
-
   it 'can format braille to correct size' do 
-    expect(@library.convert_string_to_braille("a")).to eq("0.\n..\n..\n")
-    expect(@library.convert_string_to_braille("s")).to eq(".0\n0.\n0.\n")
-    expect(@library.convert_string_to_braille("h")).to eq("0.\n00\n..\n")
-    expect(@library.convert_string_to_braille("l")).to eq("0.\n0.\n0.\n")
-    expect(@library.convert_string_to_braille("e")).to eq("0.\n.0\n..\n")
-    expect(@library.convert_string_to_braille("y")).to eq("00\n.0\n00\n")
-  end
-
-  it 'can break the line after 240 characters' do 
-    expect(@library.line_break(converted_message)).to eq()
+    expect(@EnglishBraille.convert_string_to_braille("a")).to eq("0.\n..\n..\n")
+    expect(@EnglishBraille.convert_string_to_braille("s")).to eq(".0\n0.\n0.\n")
+    expect(@EnglishBraille.convert_string_to_braille("h")).to eq("0.\n00\n..\n")
+    expect(@EnglishBraille.convert_string_to_braille("l")).to eq("0.\n0.\n0.\n")
+    expect(@EnglishBraille.convert_string_to_braille("e")).to eq("0.\n.0\n..\n")
+    expect(@EnglishBraille.convert_string_to_braille("y")).to eq("00\n.0\n00\n")
+    expect(@EnglishBraille.convert_string_to_braille("ashley")).to eq("0..00.0.0.00\n..0.000..0.0\n..0...0...00\n")
   end
 end
